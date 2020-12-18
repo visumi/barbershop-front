436286f8655c910d6060706609cc5282b7d3fe85
Isumi <v206759@dac.unicamp.br>
2020-12-18 01:52:30 -0300

    Organizado e comentado ts do dashboard

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { NbToastrService } from '@nebular/theme';
 import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
@@ -9,72 +10,51 @@ import { DashboardService } from 'src/app/services/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
+  guid = '752dbb08-cb39-4941-8e91-b2f8d10383ed'; // Guid da Barbearia Default
+
+  // Arrays de dados
   services: any[];
   clients: any[];
+  bookings = [];
+  cart = [];
 
-  selectedService: string;
-  guid = '752dbb08-cb39-4941-8e91-b2f8d10383ed';
-  avulso = false;
-  dateButton = true;
-  clearButton = true;
-  newBooking: FormGroup;
-  date = new Date();
-  today = new Date();
-  selectedBook: {
-    date: string,
+  // Variáveis de controle
+  scheduleButton = true; // Controla o botão de Reserva
+  scheduleSpinner = false; // Spinner do botao de Reservar
+  avulsoToggle = false; // Identifica se a venda é avulsa ou não
+  clearButton = true; // Controla o botão de Limpar do carrinho
+
+  // Form
+  newBooking: FormGroup; // Cria o form de Nova Reserva
+
+  // Dados úteis
+  date = new Date(); // Data que será alterada
+  today = new Date(); // Data que será usada como parâmetro
+
+  selectedBook: { // Salva qual a Reserva está selecionada no momento, ao iniciar será o valor Default
+    id: string,
+    service: string,
     client: string,
-    time: string
+    scheduledAt: string
   } = {
-    date: '00/00/0000',
+    id: '',
+    service: '',
     client: '',
-    time: '00:00'
+    scheduledAt: ''
   };
-
-  bookings = [
-    {
-      date: '12/2/2020',
-      client: 'Testificate1',
-      time: '13:00'
-    },
-    {
-      date: '13/3/2020',
-      client: 'Testificate2',
-      time: '14:00'
-    },
-    {
-      date: '13/3/2020',
-      client: 'Testificate3',
-      time: '15:00'
-    }
-  ];
-
-  cart = [
-    {
-      id: '1',
-      nome: 'Pomada para cabelo',
-      qty: 1
-    },
-    {
-      id: '2',
-      nome: 'Corte de Cabelo',
-      qty: 2
-    },
-    {
-      id: '3',
-      nome: 'Corte de Barba',
-      qty: 3
-    }
-  ];
 
   constructor(
     private formBuilder: FormBuilder,
-    private dashboardService: DashboardService
+    private dashboardService: DashboardService,
+    private toastrService: NbToastrService
   ) { }
 
   ngOnInit(): void {
+    // Carrega os arrays
     this.getClients();
     this.getServices();
 
+    // Cria o formulário
     this.newBooking = this.formBuilder.group({
       idService: '',
       idClient: '',
@@ -83,7 +63,11 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  private getClients(): void {
+  showToast(message, title, status): void { // Exibe o Toastr
+    this.toastrService.show(message, title, { status, icon: 'bulb', iconPack: 'eva' });
+  }
+
+  private getClients(): void { // Carrega os clientes
     const clientsAux = [];
     this.dashboardService.getClients(this.guid).subscribe(
       (res) => {
@@ -100,7 +84,7 @@ export class DashboardComponent implements OnInit {
     this.clients = clientsAux;
   }
 
-  private getServices(): void {
+  private getServices(): void { // Carrega os serviços
     const servicesAux = [];
     this.dashboardService.getServices(this.guid).subscribe(
       (res) => {
@@ -117,19 +101,20 @@ export class DashboardComponent implements OnInit {
     this.services = servicesAux;
   }
 
-  public handleDateChange(selectedDate): void {
+  public handleDateChange(selectedDate): void { // Converte a data para o formulário da Nova Reserva e controla o botão
     this.date = selectedDate;
     const convertedDate = this.date.toISOString().split('T')[0];
 
-    if (selectedDate < this.today) {
-      this.dateButton = true;
+    if (selectedDate < this.today) { // Se a data for menor que o dia atual, trava o botão
+      this.scheduleButton = true;
     } else {
-      this.newBooking.setValue({ idClient: '', scheduledAt: convertedDate, idService: '', time: '' });
-      this.dateButton = false;
+      this.newBooking.setValue({ idClient: '', scheduledAt: convertedDate, idService: '', time: '' }); // Preenche o formulário com a data
+      this.scheduleButton = false;
     }
   }
 
-  public createSchedule(data): void {
+  public createSchedule(data): void { // Cria uma reserva
+    this.scheduleSpinner = true;
     const book = {
       idBarberShop: this.guid,
       idService: data.idService,
@@ -138,19 +123,40 @@ export class DashboardComponent implements OnInit {
     };
 
     this.dashboardService.addSchedule(book).subscribe(
-      (res) => {
-        console.log(res);
-      }, (error) => {
-        console.log(error);
+      () => {
+        this.scheduleSpinner = false;
+        this.showToast('Reserva criada', 'Sucesso', 'success');
+      }, () => {
+        this.scheduleSpinner = false;
+        this.showToast('Problema ao criar Reserva', 'Erro', 'danger');
        }
     );
   }
 
-  public checkBook(book): void {
+  public selectBook(book): void { // Modifica qual a Reserva está selecionada no momento
     this.selectedBook = book;
   }
 
-  public teste(): void {
+  public changeAvulso(): void { // Muda os dados da Reserva selecionada de acordo com o Toggle
+    this.avulsoToggle = !this.avulsoToggle;
+    if (this.avulsoToggle === true) {
+      this.selectedBook = { // Aqui a venda é avulsa, precisa pegar data e tempo do momento do toggle
+        id: '',
+        service: '',
+        client: '',
+        scheduledAt: ''
+      };
+    } else {
+      this.selectedBook = {
+        id: '',
+        service: '',
+        client: '',
+        scheduledAt: ''
+      };
+    }
+  }
+
+  public openModal(): void { // Abre o modal para selecionar os produtos
     this.clearButton = false;
     const item = {
       id: '3',
@@ -165,7 +171,7 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  public removeItem(item): void {
+  public removeItem(item): void { // Remove um item do carrinho
     const itemFound = this.cart.find(cartItem => cartItem.id === item.id);
     if (itemFound.qty > 1) {
       itemFound.qty--;
@@ -178,25 +184,8 @@ export class DashboardComponent implements OnInit {
     console.log(this.cart);
   }
 
-  public clearCart(): void {
+  public clearCart(): void { // Limpa o carrinho
     this.cart = [];
     this.clearButton = true;
-  }
-
-  public avulsoToggle(): void {
-    this.avulso = !this.avulso;
-    if (this.avulso === true) {
-      this.selectedBook = {
-        date: '14/12/2020',
-        client: '',
-        time: '12:00'
-      };
-    } else {
-      this.selectedBook = {
-        date: '00/00/0000',
-        client: '',
-        time: '00:00'
-      };
-    }
   }
 }
